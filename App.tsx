3a4d6fb6c976bbfc0b592e284663012d61a92e6e
Vitor Nunes <stecks10@gmail.com>
2021-07-19 14:40:58 -0300

    Criando components com styled Components

--- App.tsx
+++ App.tsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Welcome } from './src/Components/Welcome';
+import { View, Text } from 'react-native';
+
+import { Dashboard } from './src/screens/Dashboard/';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Welcome title="React native bare workd type" />
-    </View>
-  );
+  return <Dashboard />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
