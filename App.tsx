07b2da546a1abc5e30435de14d46eaf2adb2bd5d
Vitor Nunes <stecks10@gmail.com>
2021-07-20 09:06:43 -0300

    Utilizando proporções em medidas

--- App.tsx
+++ App.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
 
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+
+import theme from './src/global/styles/theme';
 import { Dashboard } from './src/screens/Dashboard/';
 
 export default function App() {
-  return <Dashboard />;
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
 }
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
-
-import { Container, Title } from './styles';
+import { Text, View } from 'react-native';
+import {
+  Container,
+  Header,
+  UserInfo,
+  UserWrapper,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/80498486?v=4',
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Vitor</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
